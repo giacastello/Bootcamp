ee8ed14cb9e9d50181f7454556b9f8229e30dc05
Gia Castello <giacastello@gmail.com>
2017-10-09 12:23:10 -0300

    adding while, for and do while

---
+++ Ada/Ejercicios/Gia/lógica/array-indice.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+var semana = [50, 35, 105, 40, 25];
+var total
+for (i=0; i<semana.length; i++) { //uso la variable de control de for i para recorrer los términos.
+  console.log (semana[i]);
+  total + = semana [i]; //total + semana [i];
+}
+console.log(total);
+
+prom= total/semana.length;
---
+++ Ada/Ejercicios/Gia/lógica/temanuevo.js
@@ -0,0 +1,21 @@
+//imprime código en línea
+console.log(1,2,3);
+//imprime código uno abajo del otro
+console.log(1);
+console.log(2);
+console.log(3);
+
+
+
+
+// x++ = operador de incremento. Al valor de x, le suma uno. (x+1)
+
+//x-- = operador que resta 1 al valor de x por cada ejecución.
+x=1
+while (x<=100) {
+  console.log(x);
+  x++;
+}
+
+console.log("salí y el valor de x es:");
+console.log(´salí y el valor de X es ${X}´);
