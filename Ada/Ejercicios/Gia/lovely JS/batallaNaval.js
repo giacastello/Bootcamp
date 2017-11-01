f458e8c4e7ab061a52fa503a84a520df83495ecb
Gia Castello <giacastello@gmail.com>
2017-11-01 12:52:07 -0300

    BatallaNaval.js

--- Ada/Ejercicios/Gia/lovely JS/batallaNaval.js
+++ Ada/Ejercicios/Gia/lovely JS/batallaNaval.js
@@ -1,57 +1,88 @@
-var barcos = [
-var colA=[0,0,0,0,0,0,0,0,0,0],
-var colB=[0,0,0,0,0,0,0,0,0,0],
-var colC=[0,0,0,1,0,0,0,2,0,0],
-var colD=[0,0,0,0,0,0,0,2,0,0],
-var colE=[4,0,0,0,0,0,0,0,0,0],
-var colF=[4,0,0,0,0,0,0,0,0,0],
-var colG=[4,0,0,0,0,0,0,0,0,0],
-var colH=[4,0,0,0,0,0,0,0,0,0],
-var colI=[0,0,0,0,0,3,3,3,0,0],
-var colJ=[0,0,0,0,0,0,0,0,0,0],
-]
-/*
-- preguntoPosición (x,y)
-- busco el vlaor de la posición
-- determino el tipo de barco (1,2,3,4)
+/*- preguntoPosicion(x,y)
+- busco el valor de la posicion
+- determino tipo de barco (1,2,3,4)
+- determino hundido o tocado
 - contador de hundidos
 - contador de tocados
-*/
-barcos [x] [y]
+x e y
+barcos[x][y]*/
 
-if (barcos [x][y] == 0 || barcos [x][y] == "x"){
+var barcos= [
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,1,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,2,0,0,0,0,0],
+            [0,0,0,0,2,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,4,0],
+            [0,0,3,3,3,0,0,0,4,0],
+            [0,0,0,0,0,0,0,0,4,0],
+            [0,0,0,0,0,0,0,0,4,0]]
 
-  if(barcos[x][y] == 0){
-    barcos[x][y] = "x";
-    return "Agua";
-  }
-  return "Ya me lo dijiste";
 
-} else { //hay un barco
-  switch(barcos[x][y]){
-    case 1: contHundidos++;
-            barcos[x][y] = -1;
-    break;
+var contHundidos=0;
+var contT2=0;
+var contT3=0;
+var contT4=0;
 
-    case 2: contT2++;
-          if(contT2 == 2){
-          return "hundiste destructor";
-          contHundidos++;
-          }
-          barcos[x][y] = -2;
-    break;
+while(contHundidos<4){
 
-    case 3: contT3++;
-            if(contT3 == 3){
-            return "hundiste crucero";
-            }
-            contHundidos++;
-            barcos[x][y] = -3;
-    break;
+var x= prompt ("Ingrese una fila de 0 a 9 ");
+var y=prompt ("Ingrese una columna de 0 a 9 ");
 
-    case 4: contT4++;
+ if(barcos[x][y] == 0 || barcos[x][y] == "x"){
 
-  } break;
+	if(barcos[x][y] == 0){
+		barcos[x][y] = "x";
+		console.log("Agua");
+	} else if (barcos[x][y] = "x") {
+		console.log("Ya me lo dijiste");
+	}
+} else { // hay un barco
+
+	switch(barcos[x][y]){
+
+		case 1: contHundidos++;
+				barcos[x][y] = -1;
+				console.log("hundiste submarino");
+		        break;
+
+		case 2: contT2++;
+				barcos[x][y] = -2;
+				if(contT2 == 2){
+					contHundidos++;
+					console.log("hundiste destructor");
+				} else if (barcos[x][y] == -2){
+					console.log("Ya me lo dijiste");
+				}
+
+				break;
+
+		case 3: contT3++;
+				if(contT3 == 3){
+					contHundidos++;
+					console.log("hundiste crucero");
+				}else if (barcos[x][y] == -3){
+					console.log("Ya me lo dijiste");
+				}
+				barcos[x][y] = -3;
+				break;
+		case 4: contT4++;
+				if(contT4 == 4){
+					contHundidos++;
+					console.log("hundiste acorazado");
+				}else if (barcos[x][y] == -4){
+					console.log("Ya me lo dijiste");
+				}
+				barcos[x][y] = -4;
+				break;
+
+
+
+
+	}
 
 
 }
+}
+console.log("Ganaste");
