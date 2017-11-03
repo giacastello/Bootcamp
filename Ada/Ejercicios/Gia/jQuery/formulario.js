4a8442ccc0cb57f8297eb6326b4641668a7a7ecb
Gia Castello <giacastello@gmail.com>
2017-11-03 11:08:26 -0300

    adding jQuery

---
+++ Ada/Ejercicios/Gia/jQuery/formulario.js
@@ -0,0 +1,4 @@
+function changeColor (color){
+  var e = document.getElementById('test'); //document: ver donde estoy, y me quedo con el id test
+  e.style.color=color;// style es la propiedad del nodo html, y me paro en color
+}
