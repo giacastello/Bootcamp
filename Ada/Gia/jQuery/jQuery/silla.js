f00a882f30d680815b075f0e6390d7afb568be0a
Gia Castello <giacastello@gmail.com>
2017-11-09 11:26:15 -0300

    jQuery gatitos

---
+++ Ada/Gia/jQuery/jQuery/silla.js
@@ -0,0 +1,21 @@
+
+// tomar todo el documento cuando está cargado (ready) y llama a una función de callback
+
+// una vez cargado el documento
+$(document).ready(function(){
+//al cliquear en un elemento p
+    $("button").click(function(){
+//Ocultar ese elemento
+        $("p").hide();
+    });
+});
+
+
+    //al clickear un elemento 
+    // $('hell').click(function(){
+     
+       //ocultar ese elemento
+        //$('#texto').hide();
+   // });
+
+//});
